bda44cb726306f87b4ed81359b050d021c012357
Kevin Castillo <kevincastillo318@gmail.com>
2022-04-02 10:30:38 -0700

    added digital clock

--- list.js
+++ list.js
@@ -48,3 +48,26 @@ search.addEventListener("keyup", () => {
   const term = search.value.trim().toLowerCase();
   filterTodos(term);
 });
+
+
+//digital clock
+const clock = document.querySelector('.clock');
+
+const tick = () => {
+
+    const now = new Date();
+
+    const h = now.getHours();
+    const m = now.getMinutes();
+    const s = now.getSeconds();
+
+    const html = `
+        <span>${h}</span> : 
+        <span>${m}</span> : 
+        <span>${s}</span>
+    `;
+
+    clock.innerHTML = html
+};
+
+setInterval(tick, 1000);
